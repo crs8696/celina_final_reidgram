8cbc668af0236732cfd93adb5de12c85e0233bb6
anonymous anonymous <celina.stewart@kellogg.northwestern.edu>
2021-11-19 20:15:36 +0000

    Direct association between users and followers

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,6 +1,9 @@
 class Follower < ApplicationRecord
   # Direct associations
 
+  belongs_to :receiver,
+             :class_name => "User"
+
   belongs_to :follower,
              :class_name => "User"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :follow_requests,
+             :class_name => "Follower",
+             :foreign_key => "receiver_id",
+             :dependent => :destroy
+
   has_many   :followers,
              :foreign_key => "follower_id",
              :dependent => :destroy
