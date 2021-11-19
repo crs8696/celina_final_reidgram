5b15cf4c17a3d666dbec88ecb60b45c7f1fa37c2
anonymous anonymous <celina.stewart@kellogg.northwestern.edu>
2021-11-19 20:15:50 +0000

    Whitelist columns in Picture for ActiveAdmin

--- app/admin/pictures.rb
+++ app/admin/pictures.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Picture do
 
+ permit_params :description, :photo, :photo_owner, :place
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
